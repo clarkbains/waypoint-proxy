7697b44d035361fec2e3c8470ae8d618fd21b3bb
Clark Bains <clarkbains@gmail.com>
2022-01-10 00:15:55 -0500

    test auth proxy user attr
    Signed-off-by: Clark Bains <clarkbains@gmail.com>

--- routers/userRouter/userRouter.go
+++ routers/userRouter/userRouter.go
@@ -1,24 +1,45 @@
 package userrouter
 
 import (
+	b64 "encoding/base64"
+	"encoding/json"
 	"fmt"
-	"sync"
 	"net/http/httputil"
+	"sync"
+
 	"github.com/clarkbains/waypoint-proxy/routers"
 	"github.com/gin-gonic/gin"
 )
 
+type Groups struct {
+	GroupId string
+	GroupName string
+}
 
+type UserInfo struct {
+	Id string `json:"id"`
+	UserName string `json:"un"`
+	Discriminator int `json:"dc"`
+	Groups []Groups `json:"gp"`
+}
+func getUser (s string ) UserInfo {
+	if len(s) == 0 {
+		s = "eyJpZCI6IjYxODg4ODYxMDY5MDg5MTc5NyIsInNpZCI6W1siOTI5OTU0MTUzNjI5OTA5MDIzIiwid2F5cG9pbnQtcHJveHktdXNlciJdXX0="
+	}
+	decoded, _ := b64.StdEncoding.DecodeString(s)
+	var userData UserInfo
+	json.Unmarshal(decoded, &userData)
+	return userData
+}
 
 func Start(wg *sync.WaitGroup, port int){
 	wg.Add(port)
 	defer wg.Done()
 	ur := routers.NewRouter()
 	
-	ur.GET("/dump", func(c *gin.Context) {
-		requestDump, _ := httputil.DumpRequest(c.Request, true)
-
-		c.JSON(200, string(requestDump))
+	ur.GET("/self", func(c *gin.Context) {
+		data := getUser(c.GetHeader("x-cwdc-user"))
+		c.String(200, "id: %s, username: %s#%d", data.Id, data.UserName, data.Discriminator)
 		})
 	ur.Run(fmt.Sprintf(":%d", port))
 }
